e7d40cd1a5b68ee3ccee6199abc696b22bf7716e
Jun Hao Ng <e1406393@u.nus.edu>
2025-05-20 14:59:37 +0800

    1.2.1

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Header from './components/Header.jsx'
 import './App.css'
 import Home from './components/pages/Home.jsx'
 import Login from './components/pages/Login.jsx'
+import { Link } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0);
@@ -15,7 +16,11 @@ function App() {
           <Routes>
             <Route index path='/' element={<Home />}></Route>
             <Route path='/login' element={<Login />}></Route>
-          </Routes> 
+          </Routes>
+          <nav>
+            <p><Link to="/">Home</Link></p>
+            <Link to="/login">Login</Link>
+          </nav> 
         </BrowserRouter>
       </div>
     </main>
