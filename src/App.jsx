1dd7e8303310084b852b3e88db3a8b8c5ea34c74
Jun Hao Ng <e1406393@u.nus.edu>
2025-05-20 15:17:11 +0800

    1.2.1

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Header from './components/Header.jsx'
 import './App.css'
 import Home from './components/pages/Home.jsx'
 import Login from './components/pages/Login.jsx'
-import { Link } from 'react-router-dom'
+
 
 function App() {
   const [count, setCount] = useState(0);
@@ -17,10 +17,6 @@ function App() {
             <Route index path='/' element={<Home />}></Route>
             <Route path='/login' element={<Login />}></Route>
           </Routes>
-          <nav>
-            <p><Link to="/">Home</Link></p>
-            <Link to="/login">Login</Link>
-          </nav> 
         </BrowserRouter>
       </div>
     </main>
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
 import './Home.css';
+import { Link } from 'react-router-dom'
 
 function Home () {
-    return (<><p>DADA</p></>);
+    return (<>
+    
+    <p>DADA</p>
+    
+           <nav>
+            <p><Link to="/">Home</Link></p>
+            <Link to="/login">Login</Link>
+          </nav>
+ 
+    
+      
+      </>);
+
+          
+
 }
 
 export default Home;
