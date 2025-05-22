7963484302812adfeec5db6abb9fd0990e53e128
yitao36 <yitao36g@gmail.com>
2025-05-22 10:59:22 +0800

    v1.3 TS

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,8 @@ import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Header from './components/Header.tsx'
 import './App.css'
-<<<<<<< HEAD:src/App.jsx
-import Home from './components/pages/Home.jsx'
-import Login from './components/pages/Login.jsx'
-import NotFound from './components/pages/NotFound.jsx'
-
-=======
 import Home from './components/pages/Home.tsx'
 import Login from './components/pages/Login.tsx'
->>>>>>> feature-login:src/App.tsx
 
 function App() {
   const [count, setCount] = useState(0);
@@ -21,10 +14,9 @@ function App() {
         <BrowserRouter>
           <Header />
           <Routes>
-            <Route index path='/' element={<Home />} />
-            <Route path='/login' element={<Login />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
+            <Route index path='/' element={<Home />}></Route>
+            <Route path='/login' element={<Login />}></Route>
+          </Routes> 
         </BrowserRouter>
       </div>
     </main>
--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,32 +1,8 @@
 import React from 'react';
 import './Home.css';
-import { Link } from 'react-router-dom'
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-// import { faCoffee } from '@fortawesome/free-solid-svg-icons'
-
-
 
 function Home () {
-    return (<>
-    
-    <p>DADA</p>
-    
-           <nav>
-            <p className="box"><Link to="/">Home</Link></p>
-            <p className='box'>
-                <Link to="/login">
-                    {/* Login<FontAwesomeIcon icon={faCoffee}/> */}
-
-                </Link>
-            </p>
-          </nav>
- 
-    
-      
-      </>);
-
-          
-
+    return (<><p>DADA</p></>);
 }
 
 export default Home;
