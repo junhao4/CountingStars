233d5219b322109637e73167a2d01d08151b9938
Jun Hao Ng <e1406393@u.nus.edu>
2025-05-27 15:12:17 +0800

    added new folder for contexts

---
+++ src/components/contexts/PageTitleContext.tsx
@@ -0,0 +1,28 @@
+import { createContext, useContext, useState } from "react"
+
+ interface PageTitleProps {
+        title: string
+        setTitle: (title : string) => void;
+    }
+
+
+export const PageTitleContext = 
+    createContext<PageTitleProps>({
+        title: "",
+        setTitle: (arg0: string) => {},
+    })
+
+
+export const PageTitleProvider = ({ children }: { children: React.ReactNode }) => {
+  const [title, setTitle] = useState("Home");
+
+  return (
+    <PageTitleContext.Provider value={{ title, setTitle }}>
+      {children}
+    </PageTitleContext.Provider>
+  );
+};
+
+export const usePageTitleContext = () => useContext(PageTitleContext);
+
+
