0c4519561f20df92531f0071bd17d9d22c695cf1
Jun Hao Ng <e1406393@u.nus.edu>
2025-05-30 11:01:38 +0800

    added dashboard button to home if user is loggedin

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,12 +1,15 @@
 import { useEffect } from 'react';
 import './Home.css';
 import { usePageTitleContext } from '../contexts/PageTitleContext';
+import { useSessionContext } from '../contexts/SessionContext';
+import Button from '../Button';
 
 
 function Home () {
 
     //Set header title to Home
     const { title, setTitle } = usePageTitleContext();
+    const { session } = useSessionContext();
     
     useEffect(() => {
         console.log("Setting title to Home")
@@ -15,7 +18,14 @@ function Home () {
     }, [])
     //
 
-    return (<><p>DADA</p></>);
+    if (!session) {
+        return (<><p>DADA</p></>);
+    } else {
+        return <>
+        <p></p>
+        <Button to='/dashboard'> Dashboard </Button>
+        </>
+    }
 }
 
 export default Home;
