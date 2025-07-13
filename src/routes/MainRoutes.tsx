a15e626437d769c9ac032360cce14616dd8ba92b
Jun Hao Ng <e1406393@u.nus.edu>
2025-07-13 18:29:48 +0800

    theme context

--- src/routes/MainRoutes.tsx
+++ src/routes/MainRoutes.tsx
@@ -7,6 +7,7 @@ import LoginPage from "../pages/auth/LoginPage";
 import RegisterPage from "../pages/auth/RegisterPage";
 import ResetPasswordPage from "../pages/auth/ResetPasswordPage";
 import VerifyPage from "../pages/auth/VerifyPage";
+import ThemePage from "../pages/ThemePage";
 
 
 export default function MainRoutes() {
@@ -19,6 +20,7 @@ export default function MainRoutes() {
             <Route path="/dashboard/*" element={<ProtectedRoutes />} />
             <Route path="/forgot" element={<ForgotPasswordPage />} />
             <Route path="/reset" element={<ResetPasswordPage />} />
+            <Route path="/theme" element={<ThemePage />} />
             <Route path="*" element={<NotFound />} />
         </Routes>
     )
