620eef3fb6819bdf2fe82fff7c06701db0dd444c
yitao36 <yitao36g@gmail.com>
2025-07-22 12:25:21 +0800

    fix conflict

--- src/features/organization/users/components/UserGrid.tsx
+++ src/features/organization/users/components/UserGrid.tsx
@@ -206,11 +206,7 @@ export default function UserGrid({refresh}: UserGridProps) {
                         color="info"
                         onClick={handleDeleteUser}
                         disabled={!hasPermission<"users">(userWithOrganization,
-<<<<<<< HEAD
                             "users", "remove", { userId: row.id, organizationId: org.id, role: row.role, countOfOwners: countOfOwners })}
-=======
-                            "users", "remove", { userId: row.id, organizationId: org.id, role: row.role, countOfOwners: 0 })}
->>>>>>> testing
                     />,
                 ];
             },
