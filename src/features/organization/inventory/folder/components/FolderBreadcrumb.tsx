556d6b91910465de989e326b08560ee91c83b926
yitao36 <yitao36g@gmail.com>
2025-07-27 00:46:54 +0800

    fixed dragging

--- src/features/organization/inventory/folder/components/FolderBreadcrumb.tsx
+++ src/features/organization/inventory/folder/components/FolderBreadcrumb.tsx
@@ -6,8 +6,9 @@ import { fetchCurrentFolder, moveItemIntoFolder } from "../api/FolderApi";
 import { useAlertContext } from "../../../../../common/contexts/AlertContext";
 import type { InventoryRow } from "../hooks/useGetFolderContent";
 import { addLog } from "../../../log/api/LogApi";
-import { useSessionContext } from "../../../../../common/contexts/SessionContext";
-import { useOrgContext } from "../../../../../common/contexts/OrgContext";
+import { useSessionContext, type ValidSession } from "../../../../../common/contexts/SessionContext";
+import { useOrgContext, type ValidOrg } from "../../../../../common/contexts/OrgContext";
+import { handleGenerateAlert } from "../../../../../common/functions/ErrorAlerts";
 
 interface FolderBreadcrumbProps {
     folder: {
@@ -20,34 +21,56 @@ interface FolderBreadcrumbProps {
 export default function FolderBreadcrumb({ folder, setData }: FolderBreadcrumbProps) {
     const navigate = useNavigate()
     const { createAlert } = useAlertContext()
-    const [over, setOver] = useState(false)
-    const { user } = useSessionContext()
-    const { org } = useOrgContext()
+    const { user } = useSessionContext() as ValidSession
+    const { org } = useOrgContext() as ValidOrg
 
-    const moveIntoFolder = async (moveItem: string, destinationFolderId: number | null) => {
-        if (!validateMoveIntoFolder(moveItem, destinationFolderId).data) {
+    const [over, setOver] = useState(false)
+    
+    const moveIntoFolder = async (moveItem: string, folderId: number | null) => {
+        if (!validateMoveIntoFolder(moveItem, folderId).data) {
             return
         }
-        const item = moveItem.split(',')
-        const curr = await fetchCurrentFolder(parseInt(item[1]))
 
-        const res = await moveItemIntoFolder(item[0] as 'folder' | 'item', parseInt(item[1]), destinationFolderId)
-        if (res) {
-            setData(data => data.filter(row => !(row.type === item[0] && row.id === parseInt(item[1]))))
-            createAlert('success', "Successfully moved!")
-            const updated = await fetchCurrentFolder(parseInt(item[1]))
-            const updatedFolderName = updated?.Folders?.name ?? "Root"
-            addLog(org?.id!, "moveItem", user?.id!, parseInt(item[1]), {newLocation : updatedFolderName, oldLocation : curr?.Folders?.name!})
-        } else {
-            createAlert('error', "Failed to move!")
+        // item[0] is the type 'folder' or 'item', item[1] is the id of that item type
+        const row = moveItem.split(',')
+        const rowType = row[0] as 'folder' | 'item'
+        const rowId = parseInt(row[1])
+
+        const curr = await fetchCurrentFolder(rowId)
+        if (curr === 'itemError') {
+            handleGenerateAlert(curr, createAlert)
+            return
         }
+
+        const oldLocation = curr?.Folders?.name ?? "Root"
+
+        const res = await moveItemIntoFolder(rowType, rowId, folderId)
+        if (!res) {
+            createAlert('error', "Failed to move")
+            return
+        }
+
+        setData(data => data.filter(row => !(row.type === rowType && row.id === rowId)))
+        createAlert('success', "Successfully moved!")
+
+        if (rowType === 'folder') {
+            // Do not need to create logs for folder
+            return
+        }
+
+        const updated = await fetchCurrentFolder(rowId)
+        if (updated === 'itemError') {
+            handleGenerateAlert(updated, createAlert)
+            return
+        }
+        addLog(org.id, "moveItem", user.id, rowId, { newLocation: updated?.Folders?.name!, oldLocation })
     }
 
     return (
-        <IconButton sx={{ ...(over && {backgroundColor: 'var(--primary-alternative)'}), color: 'var(--primary)' }}
+        <IconButton sx={{ ...(over && { backgroundColor: 'var(--primary-alternative)' }), color: 'var(--primary)' }}
             onClick={() => navigate('/dashboard/organization/inventory/' + folder.id)}
             onDragEnter={() => setOver(true)} onDragLeave={() => setOver(false)}
-            onDragOver={e => e.preventDefault()}    
+            onDragOver={e => e.preventDefault()}
             onDrop={e => moveIntoFolder(e.dataTransfer.getData('id'),
                 folder.id === 'root' ? null : folder.id as number)}>
             {folder.name}
